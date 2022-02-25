758d44b14c64a05c06aef8dc721f5c3c54137606
Ciante McLemore <ciantemclemore@gmail.com>
2022-02-24 20:36:39 -0500

    Add project files.

---
+++ ChapAppServiceLibrary/IService1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChapAppServiceLibrary
+{
+    [ServiceContract]
+    public interface IService1
+    {
+      
+    }
+}
---
+++ ChatAppServiceHost/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace ChatAppServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting ChatApp Service...");
+
+            // The base address that the client will use to make request to the chat app service
+            Uri baseAddress = new Uri($"http://localhost:5000/ChatApp/");
+
+            // ServiceHost instance creation
+            //ServiceHost chatServiceHost = new ServiceHost(typeof(ChatService), baseAddress);
+
+            try
+            {
+                //chatServiceHost.
+            }
+            catch (CommunicationException ce) 
+            {
+                Console.WriteLine($"An exception occurred: {ce.Message}");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
