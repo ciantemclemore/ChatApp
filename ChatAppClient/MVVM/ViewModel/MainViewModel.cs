18209134de66ae255c3ac0a74b32be28cc63ef77
Mclemore, Ciante (US) <ciante.mclemore@lmco.com>
2022-03-08 12:46:47 -0500

    Created the client UI for the wpf application

---
+++ ChatAppClient/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAppClient.MVVM.ViewModel
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        /* Commands */
+        public RelayCommand SendCommand { get; set; }
+
+
+        //private User _user;
+        //public User SelectedUser 
+        //{
+        //    get { return _user; }
+        //    set 
+        //    {
+        //        _user = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel() 
+        {
+            // Add our messages into here
+            SendCommand = new RelayCommand(rc => { });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- ChatAppClient/MainWindow.xaml.cs
+++ ChatAppClient/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChatAppClient
 {
@@ -23,6 +11,51 @@ namespace ChatAppClient
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Allows the user to drag the window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) 
+            {
+                DragMove();
+            }
+        }
+
+        /// <summary>
+        /// Allows the user to minimize the window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e) 
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+                MainContent.Margin = new Thickness(8);
+            }
+            else 
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+                MainContent.Margin = new Thickness(0);
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+           // Disconnect user from the service
+
+            // Close the application
+            Application.Current.Shutdown();
         }
     }
 }
